53f7b04554216095c236182caeb35c5d45dfc7ce
AVTpepper <Alexander.tastad@hotmail.com>
2022-10-08 09:54:29 +0000

    declared a few variables and styles X + 0 for the game board

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+const boxes = document.getElementsByClassName("box");
+const reset = document.getElementsByClassName("reset");
+const newGame = document.getElementsByClassName("new-game");
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let boxes = document.getElementsByClassName("box");
+
+    for (let box of boxes) {
+        box.addEventListener("click", function() {
+            
+        
+        })
+
+    }
+
+})
+
 function runGame () {
 
 }
